69ee844ae256033f5f075613d6ba3039c6227846
Kaiwanyawit Chawankul <kaiwanyawit.c@thoughtworks.com>
2023-05-24 12:08:58 +0700

    add firebase

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,31 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAbOCd0H041awtGSko1VFcBudPb4zI-y0U",
+  authDomain: "simple-join.firebaseapp.com",
+  projectId: "simple-join",
+  storageBucket: "simple-join.appspot.com",
+  messagingSenderId: "69345240028",
+  appId: "1:69345240028:web:4258f1a5b5dd485de66519",
+  measurementId: "G-4NF1KN3V8S"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+//
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
