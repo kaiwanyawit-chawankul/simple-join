87661b006e2f122e070edc599364181df40c59c8
Kaiwanyawit Chawankul <kaiwanyawit.c@thoughtworks.com>
2023-05-24 13:24:25 +0700

    fix analytic

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 //
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics, initializeAnalytics } from "firebase/analytics";
+import { getAnalytics, initializeAnalytics, logEvent } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -26,7 +26,9 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-initializeAnalytics(analytics.app);
+
+logEvent(analytics, 'notification_received');
+
 //
 
 const root = ReactDOM.createRoot(
